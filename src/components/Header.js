90b8d39d24395120480dd2e88fbba40837ae1c88
77oaking <978aho@gmail.com>
2023-12-26 22:01:05 +0600

    Commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom'
+
+import Logo from '../assets/img/logo.svg'
+
 const Header = () => {
-  return <div>Header</div>;
+  return <Header className='py-6 mb-12 border-b'>
+    <div className="container mx-auto flex justify-between items-center">{/* //eta shortcut hoilo .container.mx-auto eivabe  */}
+      <Link to='/'><img src={Logo} alt="" /></Link>
+    </div>
+    <div className='flex items-center gap-6'>
+      <Link className='hover:text-violet-900 transition' to=''>Log In</Link>
+      <Link className='bg-violet-700 hover:bg-violet-800 text-white px-4 py-3 rounded-lg transition' to=''>Log In</Link>
+    </div>
+  </Header>
 };
 
 export default Header;
