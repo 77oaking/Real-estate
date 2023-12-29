00200b63bd43fa953476227fded58a23ec0fbc45
77oaking <978aho@gmail.com>
2023-12-30 03:24:36 +0600

    Commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,15 +5,16 @@ import { Link } from 'react-router-dom'
 import Logo from '../assets/img/logo.svg'
 
 const Header = () => {
-  return <Header className='py-6 mb-12 border-b'>
-    <div className="container mx-auto flex justify-between items-center">{/* //eta shortcut hoilo .container.mx-auto eivabe  */}
+  return <header className='py-6 mb-12 border-b'>
+    <div className='container mx-auto flex justify-between items-center'>{/*  eta shortcut hoilo .container.mx-auto eivabe   */}
       <Link to='/'><img src={Logo} alt="" /></Link>
+      <div className='flex items-center gap-6'>
+        <Link className='hover:text-violet-900 transition' to=''>Log In</Link>
+        <Link className='bg-violet-700 hover:bg-violet-800 text-white px-4 py-3 rounded-lg transition' to=''>Sign Up</Link>
+      </div>
     </div>
-    <div className='flex items-center gap-6'>
-      <Link className='hover:text-violet-900 transition' to=''>Log In</Link>
-      <Link className='bg-violet-700 hover:bg-violet-800 text-white px-4 py-3 rounded-lg transition' to=''>Log In</Link>
-    </div>
-  </Header>
+
+  </header>
 };
 
 export default Header;
--- src/pages/PropertyDetails.js
+++ src/pages/PropertyDetails.js
@@ -1,7 +1,35 @@
 import React from 'react';
 
+import { housesData } from '../data'
+
+import { useParams } from 'react-router-dom'
+
+import { Link } from 'react-router-dom';
+
+import { BiBed, BiBath, BiArea } from 'react-icons/bi';
 const PropertyDetails = () => {
-  return <div>PropertyDetails</div>;
+
+  const { id } = useParams();
+  console.log(id);
+
+  const house = housesData.find(house => {
+    return house.id === parseInt(id);
+  })
+
+  return <section>
+    <div className="container mx-auto min-h-[800px] mb-14">
+      <div>
+        <h2>{house.name}</h2>
+        <h2>{house.address}</h2>
+      </div>
+      <div>
+
+        <div>{house.type}</div>
+        <div>{house.country}</div>
+      </div>
+      <div>$ {house.price}</div>
+    </div>
+  </section>;
 };
 
 export default PropertyDetails;
